681e5cc0759171a003c4eb7cfdcfc09ba598e43a
AlirezaTahsili <alireza.tahsili@gmail.com>
2021-01-07 19:00:13 +0330

    Added createMessage and newMessage events

--- server/server.js
+++ server/server.js
@@ -16,6 +16,16 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
   console.log("New User Connected");
 
+  socket.emit("newMessage", {
+    from: "mike",
+    text: "Hey",
+    createdAt: 123123,
+  });
+
+  socket.on("createMessage", (message) => {
+    console.log("createMessage", message);
+  });
+
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
